48b787eccdf64085c369b2677a4919db87321cfe
Ben Taljaard <benfrancotaljaard@gmail.com>
2023-09-20 14:14:02 +0200

    import

---
+++ src/test/java/com/github/bentaljaard/demoApp/HelloIT.java
@@ -0,0 +1,8 @@
+package com.github.bentaljaard.demoApp;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloIT extends HelloTest {
+    // Execute the same tests but in packaged mode.
+}
